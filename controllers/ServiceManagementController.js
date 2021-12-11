0c811107f133f668e83103f66642158b54b39bdf
fazi1live <faziislive@gmail.com>
2021-12-11 20:05:04 +0500

    add

--- controllers/ServiceManagementController.js
+++ controllers/ServiceManagementController.js
@@ -1,19 +1,20 @@
 //Accquiring Service Management Model
 const _ServiceManagementCluster = require('../models/ServiceManagementModel');
+const fs = require('fs');
 
 const AddService = async (req, res) => {
     try {
         const _ServiceToAdd = {
-            BasicPrice:req.body.BasicPrice,
-            BasicDescription:req.body.BasicDescription,
-            StandardPrice:req.body.StandardPrice,
-            StandardDescription:req.body.StandardDescription,
-            PremiumPrice:req.body.PremiumPrice,
-            PremiumDescription:req.body.PremiumDescription,
-            MainCategory:req.body.MainCategory, 
-            Price:req.body.Price,
-            CardDescription:req.body.CardDescription, 
-            MainDescription:req.body.MainDescription, 
+            BasicPrice: req.body.BasicPrice,
+            BasicDescription: req.body.BasicDescription,
+            StandardPrice: req.body.StandardPrice,
+            StandardDescription: req.body.StandardDescription,
+            PremiumPrice: req.body.PremiumPrice,
+            PremiumDescription: req.body.PremiumDescription,
+            MainCategory: req.body.MainCategory,
+            Price: req.body.Price,
+            CardDescription: req.body.CardDescription,
+            MainDescription: req.body.MainDescription,
             ImageUrl: `/assets/Service/${req.file.filename}`,
             ImageName: req.file.originalname,
             ImageMimeType: req.file.mimetype,
@@ -21,8 +22,8 @@ const AddService = async (req, res) => {
         const _SavedData = await _ServiceManagementCluster.create(_ServiceToAdd);
         res.json({
             Message: "Service Saved Successfully",
-            Data:true,
-            Result:_SavedData
+            Data: true,
+            Result: _SavedData
         })
     } catch (error) {
         res.json({
@@ -33,20 +34,106 @@ const AddService = async (req, res) => {
     }
 }
 
-const GetAllServices = async (req,res) => {
+const GetAllServices = async (req, res) => {
     try {
         const _GetAllServices = await _ServiceManagementCluster.find();
         res.json({
-            Message:'All Services Found Successfully',
-            Data:true,
-            Result:_GetAllServices
+            Message: 'All Services Found Successfully',
+            Data: true,
+            Result: _GetAllServices
         })
     } catch (error) {
         res.json({
-            Message:error.message,
-            Data:false,
-            Result:null
+            Message: error.message,
+            Data: false,
+            Result: null
         })
     }
 }
-module.exports = { AddService, GetAllServices } 
+
+const DeleteById = async (req, res) => {
+    try {
+        const _GetServiceId = req.params._ServiceId;
+        const _GetServiceById = await _ServiceManagementCluster.findById({ _id: _GetServiceId });
+        fs.unlinkSync(`.${_GetServiceById.ImageUrl}`);
+        const _DeleteServiceById = await _ServiceManagementCluster.deleteOne({ _id: _GetServiceId });
+        res.json({
+            Message: 'Deleted Successfully',
+            Data: true,
+            Result: _DeleteServiceById
+        })
+    } catch (error) {
+        res.json({
+            Message: error.message,
+            Data: false,
+            Result: null
+        })
+    }
+}
+
+// Update Block Code
+
+const GetServiceById = async (req, res) => {
+    try {
+        const _GetServiceId = req.params._ServiceId;
+        const _GetServiceById = await _ServiceManagementCluster.findById({ _id: _GetServiceId });
+        res.json({
+            Message: "Service Found Successfully",
+            Data: true,
+            Result: _GetServiceId
+        })
+    } catch (error) {
+        res.json({
+            Message: error.message,
+            Data: false,
+            Result: null
+        })
+    }
+}
+
+const UpdateServiceById = async (req, res) => {
+    try {
+        const _GetServiceId = req.params._ServiceId;
+        const _PayloadToUpdate = req.body;
+        const _GetServiceById = await _ServiceManagementCluster.findById({ _id: _GetServiceId });
+        fs.unlinkSync(`.${_GetServiceById.ImageUrl}`);
+        const _UpdatedPayload = await _ServiceManagementCluster.updateOne(
+            { _id: _GetServiceId },
+            {
+                $set: {
+                    // _PayloadToUpdate,
+                    // ImageUrl: `/assets/Service/${req.file.filename}`,
+                    // ImageName: req.file.originalname,
+                    // ImageMimeType: req.file.mimetype,
+                    BasicPrice: req.body.BasicPrice,
+                    BasicDescription: req.body.BasicDescription,
+                    StandardPrice: req.body.StandardPrice,
+                    StandardDescription: req.body.StandardDescription,
+                    PremiumPrice: req.body.PremiumPrice,
+                    PremiumDescription: req.body.PremiumDescription,
+                    MainCategory: req.body.MainCategory,
+                    Price: req.body.Price,
+                    CardDescription: req.body.CardDescription,
+                    MainDescription: req.body.MainDescription,
+                    ImageUrl: `/assets/Service/${req.file.filename}`,
+                    ImageName: req.file.originalname,
+                    ImageMimeType: req.file.mimetype,
+                }
+            }
+        );
+        res.json({
+            Message: 'Updated Successfully',
+            Data: true,
+            Result: _UpdatedPayload
+        })
+    } catch (error) {
+        res.json({
+            Message: error.message,
+            Data: false,
+            Result: null
+        })
+    }
+}
+
+// Update Block Code
+module.exports = { AddService, GetAllServices, DeleteById, GetServiceById, UpdateServiceById } 
--- routes/ServiceManagementRoutes.js
+++ routes/ServiceManagementRoutes.js
@@ -5,11 +5,14 @@ const Router = express.Router();
 //Accquiginr Library Files
 const { UploadServiceImage } = require('../libraryfiles/UploadUploadServiceImageImage');
 //Accquiring Controllers
-const { AddService, GetAllServices } = require('../controllers/ServiceManagementController');
+const { AddService, GetAllServices, DeleteById, GetServiceById, UpdateServiceById } = require('../controllers/ServiceManagementController');
 
 
 //Routes
 Router.post('/AddService',UploadServiceImage.single('ServiceImage'),AddService);
 Router.get('/GetAllServices',GetAllServices);
+Router.delete('/DeleteById/:_ServiceId',DeleteById);
+Router.get('/GetServiceById/:_ServiceId',GetServiceById);
+Router.post('/UpdateServiceById/:_ServiceId',UploadServiceImage.single('ServiceImage'),UpdateServiceById);
 
 module.exports = Router;
